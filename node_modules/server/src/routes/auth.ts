3c8016b37861fc03206b6168f04be1414366a78d
halantrann <halantrann@gmail.com>
2025-11-08 16:58:02 -0800

    Lab 12: typo, used ~ instead of !

--- node_modules/server/src/routes/auth.ts
+++ node_modules/server/src/routes/auth.ts
@@ -38,7 +38,7 @@ router.post("/register", (req: Request, res: Response) => {
 router.post("/login", (req: Request, res: Response) => {
 	const { username, password } = req.body; // from form 
 
-	if (~username || ~password) {
+	if (!username || !password) {
 		res.status(400).send("Bad request: Invalid input data.");
 	} else {
 		credentials
--- packages/server/dist/index.js
+++ packages/server/dist/index.js
@@ -25,6 +25,7 @@ var import_dish_svc = __toESM(require("./services/dish-svc"));
 var import_express = __toESM(require("express"));
 var import_mongo = require("./services/mongo");
 var import_dishes = __toESM(require("./routes/dishes"));
+var import_auth = __toESM(require("./routes/auth"));
 (0, import_mongo.connect)("melonbowl");
 const app = (0, import_express.default)();
 const port = process.env.PORT || 3e3;
@@ -32,6 +33,7 @@ const staticDir = process.env.STATIC || "public";
 app.use(import_express.default.static(staticDir));
 app.use(import_express.default.json());
 app.use("/api/dishes", import_dishes.default);
+app.use("/auth", import_auth.default);
 app.get("/hello", (req, res) => {
   res.send("Hello, World");
 });
