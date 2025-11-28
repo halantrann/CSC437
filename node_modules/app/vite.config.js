707d6a295ecae35c835171d217457c7bb212e715
halantrann <halantrann@gmail.com>
2025-11-27 17:32:46 -0800

    Lab 13: did some wizardry to fix the app build login thing, but DONE!

--- node_modules/app/vite.config.js
+++ node_modules/app/vite.config.js
@@ -1,12 +1,20 @@
 // app/vite.config.js
+import { resolve } from 'path';
 
 export default {
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'login.html')
+      }
+    }
+  },
   server: {
     proxy: {
       "/api": "http://localhost:3000",
       "/auth": "http://localhost:3000",
       "/images": "http://localhost:3000"
     }
-  },
-  
+  }
 };
