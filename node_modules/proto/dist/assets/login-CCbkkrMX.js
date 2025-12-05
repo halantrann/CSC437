16c942eaf324e7e4d9b101075a20fb468ee6f266
halantrann <halantrann@gmail.com>
2025-12-05 11:55:39 -0800

    I JUST HAD TO RUN BUILD IN PROOT I FORGOT

--- node_modules/proto/dist/assets/login-CCbkkrMX.js
+++
@@ -1,1 +0,0 @@
-import{d as o,a as m}from"./state-CywOovjo.js";import{L as r}from"./login-form-DIXweF48.js";import"./reset.css-BZ12Mw8s.js";o({"mu-auth":m.Provider,"login-form":r});
--- packages/proto/dist/assets/main-w33ExsLm.js
+++
@@ -1,1 +0,0 @@
-import{d as e,a}from"./state-CywOovjo.js";import{H as r}from"./header-DXHvo_qu.js";e({"melon-header":r,"mu-auth":a.Provider});
